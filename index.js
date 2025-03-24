f868819b5f63667a22bdcc1c467c972d7c4b7cd3
Katerina Matejovska <katerinamatejovska@email.cz>
2025-03-24 20:29:04 +0100

    edit1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-document.body.innerHTML += 'funguju!'
+/*
+document.querySelector('#smiley1').addEventListener('click', () => {
+    const btn = document.querySelector('#smiley1');
+    btn.classList.add('btn-smiley--selected');
+  });
+  document.querySelector('#smiley2').addEventListener('click', () => {
+    const btn = document.querySelector('#smiley2');
+    btn.classList.add('btn-smiley--selected');
+  });
+  document.querySelector('#smiley3').addEventListener('click', () => {
+    const btn = document.querySelector('#smiley3');
+    btn.classList.add('btn-smiley--selected');
+  });
+  document.querySelector('#smiley4').addEventListener('click', () => {
+    const btn = document.querySelector('#smiley4');
+    btn.classList.add('btn-smiley--selected');
+  });
+  document.querySelector('#smiley5').addEventListener('click', () => {
+    const btn = document.querySelector('#smiley5');
+    btn.classList.add('btn-smiley--selected');
+  });
+    */
+
+  //toto je lepší zápis toho nahoře
+
+  /*
+  const selectSmiley = (evt) => {
+    evt.target.classList.add('btn-smiley--selected');
+  };
+  
+  document.querySelector('#smiley1').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley2').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley3').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley4').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley5').addEventListener('click', selectSmiley);
+  */
+
+  
